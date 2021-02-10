908854e9c3bb96f65f16022cd9155f3360578e76
Bekir-Can <ruikosta03@gmail.com>
2021-02-10 21:30:15 +0200

    First initialing commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+        //First:
+        System.out.println("This is group repository");//then run to get out folder in project folders(left orange colour)
+
+        //Second: We enabled VCS=>Enable Version Control Integration =>Ok
+        //Third: We selected three file of project, are .idea,out,GroupX-Practice Day3.iml.
+        //Then right click Git=>Add to.gitignore=>Add to.gitignore then ok./create
+        //Class name Main' colours is red means unversion not track yet.
+        //Then COMMIT it right up sight button tick icon
+
+    }
+}
