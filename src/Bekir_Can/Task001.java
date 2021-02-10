2b706d0db67a9353d92a7da2c889c5832a03e035
Bekir-Can <ruikosta03@gmail.com>
2021-02-10 23:31:41 +0200

    class TC001 commit group works

--- src/Bekir_Can/Task001.java
+++ src/Bekir_Can/Task001.java
@@ -5,6 +5,7 @@ public class Task001 {
     public static void main(String[] args) {
 
         System.out.println("This my Task001 class of name");
+        System.out.println("This project has to be group work not alone");
     }
 
 
