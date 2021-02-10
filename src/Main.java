745b244b77677f3b713fae11a505015b94fe65b2
Bekir-Can <ruikosta03@gmail.com>
2021-02-10 21:30:57 +0200

    First initialing commit

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
         //Third: We selected three file of project, are .idea,out,GroupX-Practice Day3.iml.
         //Then right click Git=>Add to.gitignore=>Add to.gitignore then ok./create
         //Class name Main' colours is red means unversion not track yet.
-        //Then COMMIT it right up sight button tick icon
+        //Then COMMIT it right up sight button tick icon then write commit inside the window then COMMIT.
 
     }
 }
