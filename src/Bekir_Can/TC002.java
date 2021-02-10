3ff995f2c7b0257872a00bfe45fad3a80ad4cb37
Bekir-Can <ruikosta03@gmail.com>
2021-02-10 23:44:55 +0200

    class TC001 commit more works

--- src/Bekir_Can/TC002.java
+++ src/Bekir_Can/TC002.java
@@ -4,5 +4,6 @@ public class TC002 {
     public static void main(String[] args) {
 
         System.out.println("This my Task002 class of name");
+        System.out.println("more worok here");
     }
 }
